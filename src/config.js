56d954bbaee11740de28a05563b977f57c2e89c9
YannisTrouve <45897155+YannisTrouve@users.noreply.github.com>
2020-11-16 18:09:41 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "eu-west-2",
+      BUCKET: "notes-dev-serverlessdeploymentbucket-15bims7zoecyy",
+    },
+    apiGateway: {
+      REGION: "eu-west-2",
+      URL: "https://kzm9loguy5.execute-api.eu-west-2.amazonaws.com/dev",
+    },
+    cognito: {
+      REGION: "eu-west-2",
+      USER_POOL_ID: "eu-west-2_pZCn7ppDZ",
+      APP_CLIENT_ID: "2d2k6mvl0f8ouv5s8ljf9s0pr0",
+      IDENTITY_POOL_ID: "eu-west-2_pZCn7ppDZ",
+    },
+  };
+  
+  export default config;
